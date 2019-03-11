d54d4a89b28ebfaaa7defc74964c86e8edb9ca6d
Sergio <shelcom96@gmail.com>
2019-03-12 00:49:53 +0200

    Cart

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,4 +1,5 @@
 class Instrument < ApplicationRecord
+before_destroy :not_referenced_by_any_line_item
 belongs_to :user, optional: true
 has_many   :line_items
 mount_uploader :image, ImageUploader
@@ -12,5 +13,12 @@ mount_uploader :image, ImageUploader
   BRAND = %w{ Fender Gibson Epiphone ESP Martin Dean Taylor Jackson PRS  Ibanez Charvel Washburn }
   FINISH = %w{ Black White Navy Blue Red Clear Satin Yellow Seafoam }
 CONDITION = %w{ New Excellent Mint Used Fair Poor }
+
+ def not_refereced_by_any_line_item
+    unless line_items.empty?
+      errors.add(:base, "Line items present")
+      throw :abort
+    end
+end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_04_215538) do
+ActiveRecord::Schema.define(version: 2019_03_11_222522) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 2019_03_04_215538) do
     t.integer "cart_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "quantity", default: 1
     t.index ["cart_id"], name: "index_line_items_on_cart_id"
     t.index ["instrument_id"], name: "index_line_items_on_instrument_id"
   end
